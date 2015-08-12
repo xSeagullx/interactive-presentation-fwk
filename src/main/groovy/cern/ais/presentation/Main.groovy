b956bb20a041136addc362229a24522af74fc64f
Pavel Dionisiev <xSeagullx@gmail.com>
2015-08-12 23:54:02 +0200

    Initial commit. Gradle dependencies, spring Boot, repo setup.

---
+++ src/main/groovy/cern/ais/presentation/Main.groovy
@@ -0,0 +1,19 @@
+package cern.ais.presentation
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@EnableAutoConfiguration
+class Main {
+	@RequestMapping("/")
+	String home() {
+		"Hello!"
+	}
+
+	static void main(String[] args) {
+		SpringApplication.run(Main, args);
+	}
+}
